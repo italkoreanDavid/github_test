d5fcd1d4492f75d81a2ce3c5e957cbe290647f2b
백선욱 <italkorean@gmail.com>
2022-05-19 15:38:48 +0900

    테스트 코드입니다.

---
+++ Test_App/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by seon macbook on 2022/05/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, this is app for test!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test_App/Shared/Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AppApp.swift
+//  Shared
+//
+//  Created by seon macbook on 2022/05/19.
+//
+
+import SwiftUI
+
+@main
+struct Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
